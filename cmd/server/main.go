941180ddd0b4e1fea44822548225f3db41f9dc51
dale <dale@oneunivrs.com>
2024-11-13 16:00:07 +0900

    주석 제거

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,27 +3,29 @@ package main
 import (
 	"candle-collector/internal/config"
 	"candle-collector/internal/handler"
+	"candle-collector/internal/scheduler"
+	"fmt"
 	"github.com/robfig/cron/v3"
 	"net/http"
 )
 
 var c *cron.Cron
 
-//func init() {
-//	c := cron.New(cron.WithSeconds())
-//
-//	entryID, err := c.AddFunc("2 * * * * *", scheduler.CandleUpdate)
-//	if err != nil {
-//		fmt.Println("Failed to add cron job:", err)
-//		return
-//	}
-//
-//	fmt.Println("Cron job added with Entry ID:", entryID)
-//	c.Start()
-//}
+func init() {
+	c := cron.New(cron.WithSeconds())
+
+	entryID, err := c.AddFunc("2 * * * * *", scheduler.CandleUpdate)
+	if err != nil {
+		fmt.Println("Failed to add cron job:", err)
+		return
+	}
+
+	fmt.Println("Cron job added with Entry ID:", entryID)
+	c.Start()
+}
 
 func main() {
-	//defer c.Stop()
+	defer c.Stop()
 	defer config.CloseDB()
 
 	http.ListenAndServe(":8081", handler.Handler())
